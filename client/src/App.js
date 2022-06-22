6c4407cf4ed510289044da7f62de1c9cf88025fa
Diovani B Augusto <diovanib.augusto@gmail.com>
2022-06-22 10:55:25 -0300

    add open shop button event

--- client/src/App.js
+++ client/src/App.js
@@ -14,11 +14,33 @@ import {GunsInfo} from './components/interface/GunsInfo'
 
 function App() {
   const [gunInFocus, setGunInFocus] = useState(weaponsInfo[weaponsMap.CLASSIC])
+  const bg = document.getElementById("bg");
+  const market = document.getElementById("market");
 
+
+
+  document.addEventListener("keydown",(e)=>{
+    if(e.key === "b"){
+      bg.style.filter = "blur(4px)";
+      market.style.display= "flex";
+      return;
+    }
+
+    if(e.key === "Escape"){
+      bg.style.filter = "none"
+      market.style.display= "none";
+    }
+
+    
+  })
+  
+  
   return (
     <>
-      <div className='background'></div>
-      <div className="container">
+      <div className='background' id='bg'>
+        <h1>Press B to open shop</h1>
+      </div>
+      <div className="container" id='market'>
         <div className="wrapper row gap-2">
           <PistolColumn gunHovered = {(gun)=>{setGunInFocus(gun)}}/> 
           <div className="row gap-03">{/*Armas primarias*/}
